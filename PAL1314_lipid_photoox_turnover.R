b63d7c0555bd8b3a2ed15fd8a692777c54a44939
jamesrco <james.r.collins@aya.yale.edu>
2016-11-23 13:28:44 -0500

    Initial commit PAL1314_lipid_photoox_turnover.R

---
+++ PAL1314_lipid_photoox_turnover.R
@@ -0,0 +1,9 @@
+# PAL1314_lipid_photoox_turnover.R
+
+# Purpose: Combine in situ UV irradiance data, lipid data from PAL1314_PAL1516_environmental_samples.R, and AQYs calculated in PAL1314_AQY_calc.R to calculate potential lipid turnover rates on WAP during study period
+
+# ****** Assumes all variables created by other scripts in Git repo LipidPhotoOxBox are already
+# in user's workspace; these scripts can be found at https://github.com/jamesrco/LipidPhotoOxBox
+
+# Created 11/23/16 by J.R.C.
+
