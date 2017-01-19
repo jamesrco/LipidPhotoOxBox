939b8a8430938f7a24aa30b012b43562be5812a6
jamesrco <james.r.collins@aya.yale.edu>
2017-01-18 20:43:07 -0500

    Updates for new round of absorbance measurements, DHA; includes new plots

--- LipidUV_VISAbsPlots.R
+++ LipidUV_VISAbsPlots.R
@@ -17,50 +17,52 @@
 # get current working directory so we can reset it later
 initial.wd = getwd()
 
+# load in "older" (fall 2016) data
 # set wd to data location
-setwd("/Users/jrcollins/Code/LipidPhotoOxBox/data/raw/Evolution_300/lipids_in_MeOH") 
+setwd("/Users/jrcollins/Code/LipidPhotoOxBox/data/raw/Evolution_300/lipids_in_MeOH_initial") 
 
 # read in data, concatenate into single data frame
 # original profiles made at 1.113 mM concentration on 10 Aug 2016
-PC22_6 = read.csv("PC_22-6_in_MeOH_1.113mM.csv", 
+PC22_6_init = read.csv("PC_22-6_in_MeOH_1.113mM.csv", 
                   stringsAsFactors = FALSE)
-PC22_1 = read.csv("PC_22-1_in_MeOH_1.113mM.csv", 
+PC22_1_init = read.csv("PC_22-1_in_MeOH_1.113mM.csv", 
                         stringsAsFactors = FALSE)
-PC22_0 = read.csv("PC_22-0_in_MeOH,heated_1.113mM.csv", 
+PC22_0_init = read.csv("PC_22-0_in_MeOH,heated_1.113mM.csv", 
                   stringsAsFactors = FALSE)
 
 # also, load in another 22:6 profile made on 26 Oct 2016 at much diluted 
 # concentration (spec appeared to be saturated in original run)
 # note that these were only collected at unit wavelength resolution
 
-PC22_6_dil = read.csv("PC_22-6_in_MeOH_diluted_0.0027825mM.csv", 
+# **** these initial PC 22:6 data are highly suspect **** 
+PC22_6_dil_init = read.csv("PC_22-6_in_MeOH_diluted_0.0027825mM.csv", 
                   stringsAsFactors = FALSE)
-PC22_6_dil = PC22_6_dil[-c(1:8),]
+PC22_6_dil_init = PC22_6_dil_init[-c(1:8),]
 
 # convert fields to numeric
 
-LipidAbsData = cbind(as.numeric(as.character(PC22_6[,1])),
-                     as.numeric(as.character(PC22_6[,2])),
-                     as.numeric(as.character(PC22_1[,2])),
-                     as.numeric(as.character(PC22_0[,2])))
+LipidAbsData_init = cbind(as.numeric(as.character(PC22_6_init[,1])),
+                     as.numeric(as.character(PC22_6_init[,2])),
+                     as.numeric(as.character(PC22_1_init[,2])),
+                     as.numeric(as.character(PC22_0_init[,2])))
 
-LipidAbsData = as.data.frame(LipidAbsData[-c(1:8),]) # get rid of some unneeded header info
+LipidAbsData_init = as.data.frame(LipidAbsData_init[-c(1:8),]) # get rid of some unneeded header info
 
-colnames(LipidAbsData) = c("lambda_nm",
+colnames(LipidAbsData_init) = c("lambda_nm",
                            "abs_PC22_6",
                            "abs_PC22_1",
                            "abs_PC22_0")
 
 # append diluted 22:6 data, requires jerry-rigging since collected at different interval
 
-LipidAbsData$abs_PC22_6_dil = NA
+LipidAbsData_init$abs_PC22_6_dil_init = NA
 
-for (i in 1:nrow(LipidAbsData)) {
+for (i in 1:nrow(LipidAbsData_init)) {
   
-  if (length(PC22_6_dil$scan026[PC22_6_dil$Batch==LipidAbsData$lambda_nm[i]])>0) {
+  if (length(PC22_6_dil_init$scan026[PC22_6_dil_init$Batch==LipidAbsData_init$lambda_nm[i]])>0) {
     
-    LipidAbsData$abs_PC22_6_dil[i] = 
-      as.numeric(PC22_6_dil$scan026[PC22_6_dil$Batch==LipidAbsData$lambda_nm[i]])
+    LipidAbsData_init$abs_PC22_6_dil_init[i] = 
+      as.numeric(PC22_6_dil_init$scan026[PC22_6_dil_init$Batch==LipidAbsData_init$lambda_nm[i]])
     
   }
   
@@ -69,24 +71,77 @@ for (i in 1:nrow(LipidAbsData)) {
 # calculate decadic molar extinction coefficients (epsilon, in per M per cm)
 
 # specify some constants
-pathlength_cm = 10 # pathlength of cuvette used, in cm (same for both dates)
+pathlength_cm = 10 # pathlength of cuvette used, in cm (same for all dates)
 molarity_mM_20160810 = 1.113 # molarity of lipids in MeOH (original conc. on 10 Aug 16)
 molarity_mM_20161026 = 0.0027825 # molarity of diluted 22:6 sol'n evaluated on 26 Oct 16
 molarity_M_20160810 = molarity_mM_20160810/1000 # molarity, in mol/L
 molarity_M_20161026 = molarity_mM_20161026/1000 # molarity, in mol/L
 
 # calculations
-LipidAbsData$epsilon_M_cm_PC22_6 = LipidAbsData$abs_PC22_6/(molarity_M_20160810*pathlength_cm)
-LipidAbsData$epsilon_M_cm_PC22_1 = LipidAbsData$abs_PC22_1/(molarity_M_20160810*pathlength_cm)
-LipidAbsData$epsilon_M_cm_PC22_0 = LipidAbsData$abs_PC22_0/(molarity_M_20160810*pathlength_cm)
+LipidAbsData_init$epsilon_M_cm_PC22_6 = LipidAbsData_init$abs_PC22_6/(molarity_M_20160810*pathlength_cm)
+LipidAbsData_init$epsilon_M_cm_PC22_1 = LipidAbsData_init$abs_PC22_1/(molarity_M_20160810*pathlength_cm)
+LipidAbsData_init$epsilon_M_cm_PC22_0 = LipidAbsData_init$abs_PC22_0/(molarity_M_20160810*pathlength_cm)
 # require a small baseline correction for the second batch of 22:6 data
-LipidAbsData$epsilon_M_cm_PC22_6_dil = LipidAbsData$abs_PC22_6_dil/(molarity_M_20161026*pathlength_cm)
+LipidAbsData_init$epsilon_M_cm_PC22_6_dil = LipidAbsData_init$abs_PC22_6_dil/(molarity_M_20161026*pathlength_cm)
 
-# save the data file (we'll need it later)
+# reset the working directory
+setwd(initial.wd)
 
-save(LipidAbsData, file = "PC_lipid_abs_data.RData")
+# load in new (verification run) Jan 2017 data
+# set wd to data location
+setwd("/Users/jrcollins/Code/LipidPhotoOxBox/data/raw/Evolution_300/lipids_in_MeOH_rerun_Jan2017") 
+
+# read in data, concatenate into single data frame
+# verification run, including DHA, of new standards from Avanti and Cayman Chem run on 17 Jan 2017
+# didn't run PC 22:0
+# included a dilution series for PC 22-6
+PC22_6_011387mM_Jan17 = read.csv("PC_22-6_in_MeOH_0.11387mM_second_run.csv", 
+                       stringsAsFactors = FALSE)
+PC22_6_01576055mM_Jan17 = read.csv("PC_22-6_in_MeOH_0.1576055mM.csv", 
+                                 stringsAsFactors = FALSE)
+PC22_6_005679mM_Jan17 = read.csv("PC_22-6_in_MeOH_0.05679mM.csv", 
+                                   stringsAsFactors = FALSE)
+PC22_6_00285mM_Jan17 = read.csv("PC_22-6_in_MeOH_0.0285mM.csv", 
+                                   stringsAsFactors = FALSE)
+PC22_1_Jan17 = read.csv("PC_22-1_in_MeOH_1.113mM.csv", 
+                       stringsAsFactors = FALSE)
+DHA_01032mM_Jan17 = read.csv("DHA_in_MeOH_0.1032mM.csv", 
+                       stringsAsFactors = FALSE)
+
+# convert fields to numeric
+
+LipidAbsData_Jan17 = cbind(as.numeric(as.character(PC22_6_011387mM_Jan17[,1])),
+                          as.numeric(as.character(PC22_6_011387mM_Jan17[,2])),
+                          as.numeric(as.character(PC22_6_01576055mM_Jan17[,2])),
+                          as.numeric(as.character(PC22_6_005679mM_Jan17[,2])),
+                          as.numeric(as.character(PC22_6_00285mM_Jan17[,2])),
+                          as.numeric(as.character(PC22_1_Jan17[,2])),
+                          as.numeric(as.character(DHA_01032mM_Jan17[,2])))
+
+LipidAbsData_Jan17 = as.data.frame(LipidAbsData_Jan17[-c(1:8),]) # get rid of some unneeded header info
+
+colnames(LipidAbsData_Jan17) = c("lambda_nm",
+                                "abs_PC22_6_011387mM",
+                                "abs_PC22_6_01576055mM",
+                                "abs_PC22_6_005679mM",
+                                "abs_PC22_6_00285mM",
+                                "abs_PC22_1_1113mM",
+                                "abs_DHA_01032mM")
+
+# calculate decadic molar extinction coefficients (epsilon, in per M per cm)
+
+# calculations
+# concentrations must be in mol/L
+LipidAbsData_Jan17$epsilon_M_cm_PC22_6_011387mM = LipidAbsData_Jan17$abs_PC22_6_011387mM/((0.11387/1000)*pathlength_cm)
+LipidAbsData_Jan17$epsilon_M_cm_PC22_6_01576055mM = LipidAbsData_Jan17$abs_PC22_6_01576055mM/((0.1576055/1000)*pathlength_cm)
+LipidAbsData_Jan17$epsilon_M_cm_PC22_6_005679mM = LipidAbsData_Jan17$abs_PC22_6_005679mM/((0.05679/1000)*pathlength_cm)
+LipidAbsData_Jan17$epsilon_M_cm_PC22_6_00285mM = LipidAbsData_Jan17$abs_PC22_6_00285mM/((0.0285/1000)*pathlength_cm)
+LipidAbsData_Jan17$epsilon_M_cm_PC22_1_1113mM = LipidAbsData_Jan17$abs_PC22_1_1113mM/((1.113/1000)*pathlength_cm)
+LipidAbsData_Jan17$epsilon_M_cm_DHA_01032mM = LipidAbsData_Jan17$abs_DHA_01032mM/((0.1032/1000)*pathlength_cm)
 
 # make plots for liposome experiment chapter
+
+# first, using older fall 2016 data
 
 # absorbance, 22:0 and 22:1
 
@@ -100,11 +155,11 @@ pdf(file = "PCLipidAbs_22-0,22-1.pdf",
     bg = "white")
 
 par(mar=c(5,5,1,1))
-plot(LipidAbsData$lambda_nm,LipidAbsData$abs_PC22_0,"l",
+plot(LipidAbsData_init$lambda_nm,LipidAbsData_init$abs_PC22_0,"l",
      col = absPlotCol[1], lty = absPlotLty[1], lwd = "1.5",
      ylim = c(0,1.75), xlim = c(225,500), ylab = "Absorbance",
      xlab = "Wavelength (nm)")
-lines(LipidAbsData$lambda_nm,LipidAbsData$abs_PC22_1,
+lines(LipidAbsData_init$lambda_nm,LipidAbsData_init$abs_PC22_1,
       col = absPlotCol[2], lty = absPlotLty[2], lwd = "1.5")
 
 legend(x = 450, y = 1, bty = "n",
@@ -125,8 +180,8 @@ pdf(file = "PCLipidAbs_22-6.pdf",
     bg = "white")
 
 par(mar=c(5,5,1,1))
-plot(LipidAbsData$lambda_nm[seq(1,nrow(LipidAbsData),5)],
-     LipidAbsData$abs_PC22_6_dil[seq(1,nrow(LipidAbsData),5)],"l",
+plot(LipidAbsData_init$lambda_nm[seq(1,nrow(LipidAbsData_init),5)],
+     LipidAbsData_init$abs_PC22_6_dil[seq(1,nrow(LipidAbsData_init),5)],"l",
      col = absPlotCol[3], lty = absPlotLty[3], lwd = "1.5",
      ylim = c(0,0.75), xlim = c(225,500), ylab = "Absorbance",
      xlab = "Wavelength (nm)")
@@ -146,14 +201,14 @@ pdf(file = "PCLipidAbs_inset_290_315.pdf",
     bg = "white")
 
 par(mar=c(5,5,1,1))
-plot(LipidAbsData$lambda_nm,LipidAbsData$abs_PC22_0,"l",
+plot(LipidAbsData_init$lambda_nm,LipidAbsData_init$abs_PC22_0,"l",
      col = absPlotCol[1], lty = absPlotLty[1], lwd = "1.5",
      ylim = c(0,3.5), xlim = c(290,315), ylab = "Absorbance",
      xlab = "Wavelength (nm)")
-lines(LipidAbsData$lambda_nm,LipidAbsData$abs_PC22_1,
+lines(LipidAbsData_init$lambda_nm,LipidAbsData_init$abs_PC22_1,
       col = absPlotCol[2], lty = absPlotLty[2], lwd = "1.5")
-lines(LipidAbsData$lambda_nm[seq(1,nrow(LipidAbsData),5)],
-      LipidAbsData$abs_PC22_6_dil[seq(1,nrow(LipidAbsData),5)],
+lines(LipidAbsData_init$lambda_nm[seq(1,nrow(LipidAbsData_init),5)],
+      LipidAbsData_init$abs_PC22_6_dil[seq(1,nrow(LipidAbsData_init),5)],
       col = absPlotCol[3], lty = absPlotLty[3], lwd = "1.5")
 
 dev.off()
@@ -170,15 +225,15 @@ pdf(file = "PCLipidMolExtCoeff.pdf",
     bg = "white")
 
 par(mar=c(5,5,1,1))
-plot(LipidAbsData$lambda_nm,log(LipidAbsData$epsilon_M_cm_PC22_0),"l",
+plot(LipidAbsData_init$lambda_nm,log(LipidAbsData_init$epsilon_M_cm_PC22_0),"l",
      col = absPlotCol[1], lty = absPlotLty[1], lwd = "1.5",
      ylim = c(0,10), xlim = c(225,500),
      ylab = expression(paste("log ",epsilon[i]," (",M^-1," ",cm^-1,")")),
      xlab = "Wavelength (nm)")
-lines(LipidAbsData$lambda_nm,log(LipidAbsData$epsilon_M_cm_PC22_1),
+lines(LipidAbsData_init$lambda_nm,log(LipidAbsData_init$epsilon_M_cm_PC22_1),
       col = absPlotCol[2], lty = absPlotLty[2], lwd = "1.5")
-lines(LipidAbsData$lambda_nm[seq(1,nrow(LipidAbsData),5)],
-      log(LipidAbsData$epsilon_M_cm_PC22_6_dil[seq(1,nrow(LipidAbsData),5)]),
+lines(LipidAbsData_init$lambda_nm[seq(1,nrow(LipidAbsData_init),5)],
+      log(LipidAbsData_init$epsilon_M_cm_PC22_6_dil[seq(1,nrow(LipidAbsData_init),5)]),
       col = absPlotCol[3], lty = absPlotLty[3], lwd = "1.5")
 
 legend(x = 400, y = 8, bty = "n",
@@ -187,7 +242,139 @@ legend(x = 400, y = 8, bty = "n",
 
 dev.off()
 
+# now, using Jan 2017 data
+# will still have to use PC 22:0 data from the fall 2016 run since we didn't re-run PC 22:0
+
+# absorbance, 22:0 and 22:1
+
+absPlotCol = hsv(c(0.2, 0.57, 0.95), 1, 1, 0.8) # define colors
+absPlotLty = c("solid","dashed","dotdash") # define lty
+
+par(oma=c(0,0,0,0)) # set margins; large dataset seems to require this
+
+pdf(file = "PCLipidAbs_22-0,22-1.pdf",
+    width = 8, height = 6, pointsize = 12,
+    bg = "white")
+
+par(mar=c(5,5,1,1))
+plot(LipidAbsData_init$lambda_nm,LipidAbsData_init$abs_PC22_0,"l",
+     col = absPlotCol[1], lty = absPlotLty[1], lwd = "1.5",
+     ylim = c(0,1.75), xlim = c(225,500), ylab = "Absorbance",
+     xlab = "Wavelength (nm)")
+lines(LipidAbsData_Jan17$lambda_nm,LipidAbsData_Jan17$abs_PC22_1_1113mM,
+      col = absPlotCol[2], lty = absPlotLty[2], lwd = "1.5")
+
+legend(x = 450, y = 1, bty = "n",
+       legend = c("22:0/22:0 PC","22:1/22:1 PC","22:6/22:6 PC (1:400 dilution"),
+       col = absPlotCol, lty = absPlotLty, lwd = 2)
+
+dev.off()
+
+# absorbance, 22:6
+
+# abs_PC22_6_011387mM
+# abs_PC22_6_01576055mM
+# abs_PC22_6_005679mM
+# abs_PC22_6_00285mM
+# abs_PC22_1_1113mM
+# abs_DHA_01032mM
+
+absPlotCol = hsv(c(0.1, 0.35, 0.6, 0.85), 1, 1, 0.8) # define colors
+absPlotLty = c("solid","dashed","dotdash") # define lty
+
+par(oma=c(0,0,0,0)) # set margins; large dataset seems to require this
+
+pdf(file = "PCLipidAbs_22-6.pdf",
+    width = 8, height = 6, pointsize = 12,
+    bg = "white")
+
+par(mar=c(5,5,1,1))
+plot(LipidAbsData_Jan17$lambda_nm,LipidAbsData_Jan17$abs_PC22_6_011387mM,"l",
+     col = absPlotCol[3], lty = absPlotLty[3], lwd = "1.5",
+     ylim = c(0,1.1), xlim = c(225,500), ylab = "Absorbance",
+     xlab = "Wavelength (nm)")
+lines(LipidAbsData_Jan17$lambda_nm,LipidAbsData_Jan17$abs_DHA_01032mM,
+      col = absPlotCol[4], lty = absPlotLty[4], lwd = "1.5")
+
+legend(x = 450, y = 2, bty = "n",
+       legend = c("22:0/22:0 PC","22:1/22:1 PC","22:6/22:6 PC (1:400 dilution"),
+       col = absPlotCol, lty = absPlotLty, lwd = 2)
+
+dev.off()
+
+# absorbance, DHA
+
+# abs_PC22_6_011387mM
+# abs_PC22_6_01576055mM
+# abs_PC22_6_005679mM
+# abs_PC22_6_00285mM
+# abs_PC22_1_1113mM
+# abs_DHA_01032mM
+
+absPlotCol = hsv(c(0.2, 0.57, 0.95), 1, 1, 0.8) # define colors
+absPlotLty = c("solid","dashed","dotdash") # define lty
+
+par(oma=c(0,0,0,0)) # set margins; large dataset seems to require this
+
+pdf(file = "PCLipidAbs_DHA.pdf",
+    width = 8, height = 6, pointsize = 12,
+    bg = "white")
+
+par(mar=c(5,5,1,1))
+plot(LipidAbsData_Jan17$lambda_nm,LipidAbsData_Jan17$abs_DHA_01032mM,"l",
+     col = absPlotCol[3], lty = absPlotLty[3], lwd = "1.5",
+     ylim = c(0,1.1), xlim = c(225,500), ylab = "Absorbance",
+     xlab = "Wavelength (nm)")
+
+dev.off()
+
+
+# decadic molar extinction coefficients
+
+# epsilon_M_cm_PC22_6_011387mM
+# epsilon_M_cm_PC22_6_01576055mM
+# epsilon_M_cm_PC22_6_005679mM
+# epsilon_M_cm_PC22_6_00285mM
+# epsilon_M_cm_PC22_1_1113mM
+# epsilon_M_cm_DHA_01032mM
+
+absPlotCol = hsv(c(0.1, 0.35, 0.6, 0.85), 1, 1, 0.8) # define colors
+absPlotLty = c("solid","dashed","dotdash","dotted") # define lty
+
+par(oma=c(0,0,0,0)) # set margins; large dataset seems to require this
+
+pdf(file = "PCLipidMolExtCoeff.pdf",
+    width = 8, height = 6, pointsize = 12,
+    bg = "white")
+
+par(mar=c(5,5,1,1))
+plot(LipidAbsData_init$lambda_nm,log(LipidAbsData_init$epsilon_M_cm_PC22_0),"l",
+     col = absPlotCol[1], lty = absPlotLty[1], lwd = "1.5",
+     ylim = c(0,10), xlim = c(225,500),
+     ylab = expression(paste("log ",epsilon[i]," (",M^-1," ",cm^-1,")")),
+     xlab = "Wavelength (nm)")
+lines(LipidAbsData_init$lambda_nm,log(LipidAbsData_init$epsilon_M_cm_PC22_1),
+      col = absPlotCol[2], lty = absPlotLty[2], lwd = "1.5")
+ lines(LipidAbsData_Jan17$lambda_nm,log(LipidAbsData_Jan17$epsilon_M_cm_PC22_6_011387mM),
+       col = absPlotCol[3], lty = absPlotLty[3], lwd = "1.5")
+lines(LipidAbsData_Jan17$lambda_nm,log(LipidAbsData_Jan17$epsilon_M_cm_DHA_01032mM),
+      col = absPlotCol[4], lty = absPlotLty[4], lwd = "1.5")
+legend(x = 400, y = 8, bty = "n",
+       legend = c("22:0/22:0 PC","22:1/22:1 PC","22:6/22:6 PC"),
+       col = absPlotCol, lty = absPlotLty, lwd = 2)
+
+dev.off()
+
+# save a data object containing the combined, validated extinction results reported in thesis/manuscript
+LipidAbsData =
+  LipidAbsData_Jan17[,-c(2:7,9:12)]
+LipidAbsData$epsilon_M_cm_PC22_1 = LipidAbsData_init$epsilon_M_cm_PC22_1[1:1551]
+LipidAbsData$epsilon_M_cm_PC22_0 = LipidAbsData_init$epsilon_M_cm_PC22_0[1:1551]
+colnames(LipidAbsData)[2:3] = c("epsilon_M_cm_PC22_6","epsilon_M_cm_DHA")
+
+save(LipidAbsData,file = "PC_lipid_UV-VIS_molar_extinction_data.RData")
+
 # some data
 
 # wavelength of 22:6 absorbance maximum
-LipidAbsData$lambda_nm[LipidAbsData$abs_PC22_6_dil==max(LipidAbsData$abs_PC22_6_dil, na.rm =T)]
+  LipidAbsData_Jan17$lambda_nm[LipidAbsData_Jan17$abs_PC22_6_011387mM==max(LipidAbsData_Jan17$abs_PC22_6_011387mM, na.rm =T)]
